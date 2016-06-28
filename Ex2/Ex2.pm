0d2b12aef88b7e7093aace524801535acc2e8ebc
Santiago <santiago.pereztorre@gmail.com>
2016-06-27 22:18:19 -0300

    Cambio el Ex2 para que reciba por parametro el nombre del archivo

--- Ex2/Ex2.pm
+++ Ex2/Ex2.pm
@@ -2,11 +2,13 @@
 
 use Bio::Tools::Run::StandAloneBlastPlus;
 
+my $filename = $ARGV[0];
+
 $fac = Bio::Tools::Run::StandAloneBlastPlus->new(
   -db_name => 'swissprot'
 );
 
-$seqio = Bio::SeqIO->new(-file => "orf2.fa", -format => "fasta" );
+$seqio = Bio::SeqIO->new(-file => $filename, -format => "fasta" );
 
 $result = $fac->blastp( -query => $seqio, -outfile => 'blast.out');
 
